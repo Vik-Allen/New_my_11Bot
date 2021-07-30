1f965c29b669607753b8a1ed6afeaacc2fbd922c
Vik-Allen <88082358+Vik-Allen@users.noreply.github.com>
2021-07-30 13:45:08 +0300

    Delete Bot.py

--- Bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-import Bot2
-bot=telebot.TeleBot('1921095688:AAGBKHRe46VwyVs1xknEBXzYTZIGtH5xb0A');
-@bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_message(message.chat.id, message.text)
-    
-#RUN
-bot.polling(none_stop=True)
